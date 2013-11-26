75409b8bc9024f74be88db4d37ba4de54c1ca1d0
dwgill <daniel.gill@my.wheaton.edu>
2013-11-26 09:53:21 -0600

    Implement inflector

--- declinare/inflictor/inflector.py
+++ declinare/inflictor/inflector.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from collections import namedtuple
 from collections import defaultdict
 
-Case = namedtuple('Case', ['name','suffix','suffix_length'])
+Inflection = namedtuple('Inflection', ['name','suffix','suffix_length'])
 
 _noun_cases = {}
 _max_suffix_length = 4
@@ -23,56 +23,20 @@ def get_cases(word):
         for case in get(word[-length:]):
             yield case
 
-_case('first_nom_sg', 'a')
-_case('first_gen_sg', 'ae')
-_case('first_dat_sg', 'ae')
-_case('first_acc_sg', 'am')
-_case('first_abl_sg', 'a')
-_case('first_nom_pl', 'ae')
-_case('first_gen_pl', 'arum')
-_case('first_dat_pl', 'is')
-_case('first_acc_pl', 'as')
-_case('first_abl_pl', 'is')
-_case('second_nom_sg', 'us')
-# Second delension words can sometimes have irregular nominative forms
-_case('second_nom_sg', '')
-_case('second_gen_sg', 'i')
-_case('second_dat_sg', 'o')
-_case('second_acc_sg', 'um')
-_case('second_abl_sg', 'o')
-_case('second_nom_pl', 'i')
-_case('second_gen_pl', 'orum')
-_case('second_dat_pl', 'is')
-_case('second_acc_pl', 'os')
-_case('second_abl_pl', 'is')
-# Third declension nominative signulars are always irregular
-_case('third_nom_sg', '')
-_case('third_gen_sg', 'is')
-_case('third_dat_sg', 'i')
-_case('third_acc_sg', 'em')
-_case('third_abl_sg', 'e')
-_case('third_nom_pl', 'es')
-_case('third_gen_pl', 'um')
-_case('third_dat_pl', 'ibus')
-_case('third_acc_pl', 'es')
-_case('third_abl_pl', 'ibus')
-_case('fourth_nom_sg', 'us')
-_case('fourth_gen_sg', 'us')
-_case('fourth_dat_sg', 'ui')
-_case('fourth_acc_sg', 'um')
-_case('fourth_abl_sg', 'u')
-_case('fourth_nom_pl', 'us')
-_case('fourth_gen_pl', 'uum')
-_case('fourth_dat_pl', 'ibus')
-_case('fourth_acc_pl', 'us')
-_case('fourth_abl_pl', 'ibus')
-_case('fifth_nom_sg', 'es')
-_case('fifth_gen_sg', 'ei')
-_case('fifth_dat_sg', 'ei')
-_case('fifth_acc_sg', 'em')
-_case('fifth_abl_sg', 'e')
-_case('fifth_nom_pl', 'es')
-_case('fifth_gen_pl', 'erum')
-_case('fifth_dat_pl', 'ebus')
-_case('fifth_acc_pl', 'es')
-_case('fifth_abl_pl', 'ebus')
+class Inflector(object):
+    def __init__(self, inflections, max_length):
+        self.inflections = {}
+        self.max_length = max_length
+        def make_inflection(name, suffix):
+            inflection = Inflection(name, suffix, len(suffix))
+            self.inflections.setdefault(suffix, []).append(inflection)
+
+    def inflect(self, word):
+        inflections = []
+        get = lambda suffix: self.inflections.get(suffix,[])
+
+        inflections.extend(get(''))
+        for length in range(1,min(self.max_length + 1, len(word))):
+            inflections.extend(get(word[-length:]))
+
+        return inflections
